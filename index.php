14b3b6fdbf43b6b89034a35aa8e0b4358d13499f
abdelghafour aouad <abdelghafour.aouad0@gmail.com>
2023-01-01 21:08:12 +0100

    update index

--- index.php
+++ index.php
@@ -1,23 +1,73 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '', 'quizizy');
+require_once 'classes/Quiz.php';
+$n = new Quiz;
+$n->getQuiz();
+?>
 
-if ($mysqli->connect_errno != 0) {
-  echo $mysqli->connect_error;
-  exit();
-}
-$sql = "SELECT id,quiz_id,question FROM questions";
-$results = $mysqli->query($sql);
-foreach ($results as $question) {
-  $id = $question['id'];
-  $sql = "SELECT id, answer FROM answers where question_id=$id";
-  $re = $mysqli->query($sql);
-  $answers = [];
-  foreach ($re as $answer) {
-    $answers[] = $answer;
-  }
-  $question['answers'] = $answers;
-  $questions[] = $question;
-}
+<!DOCTYPE html>
+<html lang="en">
 
-$encoded_data = json_encode($questions, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-file_put_contents('data.json', $encoded_data);
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <title>Quizizy</title>
+</head>
+
+<body class="container">
+  <section id="welcome">
+    <form>
+      <div class="segment">
+        <h1>Welcome to QUIZIZY</h1>
+      </div>
+      <label>
+        <!-- <div class="button" onmousedown="">Click me!</div> -->
+        <input type="text" placeholder="Full name" id="full_name" />
+      </label>
+      <button class="red" type="button" onclick="start()"><i class="icon ion-md-lock"></i>Start</button>
+    </form>
+  </section>
+
+  <section id="questions">
+    <div class="grid-menu">
+      <h4 id="progress">1/10</h4>
+      <!-- <div class="progress">
+        <div class="progress__fill"></div>
+      </div> -->
+      <h3 id="timer">30 s</h3>
+      <h4 id="name">Name</h4>
+    </div>
+    <h4 id="question"></h4>
+    <div class="wrapper">
+      <input type="hidden" id="id" value="" />
+      <button id="answer1" class="d-flex btn neumorphic-btn neumorphic-checkbox">
+        <p></p>
+        <div type="button"></div>
+      </button>
+      <button id="answer2" class="d-flex btn neumorphic-btn neumorphic-checkbox">
+        <p></p>
+        <div type="button"></div>
+      </button>
+      <button id="answer3" class="d-flex btn neumorphic-btn neumorphic-checkbox">
+        <p></p>
+        <div type="button"></div>
+      </button>
+      <button id="answer4" class="d-flex btn neumorphic-btn neumorphic-checkbox">
+        <p></p>
+        <div type="button"></div>
+      </button>
+    </div>
+    <button id="next">NEXT</button>
+  </section>
+  <!-- <section id="results"> -->
+  <section id="results" onmousedown="party.confetti(this)">
+    <span id="score">Your score is <span>0/10</span></span>
+    <div id="result_questions"></div>
+  </section>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/party-js@latest/bundle/party.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+
+</html>
